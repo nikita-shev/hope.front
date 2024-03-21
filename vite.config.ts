d235b96cee87321983e28aecb98849ddb6e5e820
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-03-21 15:11:00 +0400

    config: Add alias.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@layout': path.resolve(__dirname, './src/layout'),
+            '@pages': path.resolve(__dirname, './src/pages'),
+            '@store': path.resolve(__dirname, './src/store'),
+            '@routers': path.resolve(__dirname, './src/routers'),
+            '@services': path.resolve(__dirname, './src/services'),
+            '@utils': path.resolve(__dirname, './src/utils')
+        }
+    }
+    // base: './'
 });
