bed18d06519aabb22bb7246f403f3f3731b0935f
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-04-07 23:31:07 +0400

    bug: Refactor method "validateEmailField" and its tests.

--- src/components/TextField/utils/validators/methods/validateEmailField/validateEmailField.test.ts
+++ src/components/TextField/utils/validators/methods/validateEmailField/validateEmailField.test.ts
@@ -5,14 +5,14 @@ describe('Method validateEmailField()', () => {
         const { isError, message } = validateEmailField('_test~email@q.onlyok');
 
         expect(isError).toBeTruthy();
-        expect(message).toBe('Неверный ввод!');
+        expect(message).toBe('Неверный ввод');
     });
 
     test('should run with the error "Empty field"', () => {
         const { isError, message } = validateEmailField('');
 
         expect(isError).toBeTruthy();
-        expect(message).toBe('Пустое поле!');
+        expect(message).toBe('Пустое поле');
     });
 
     test('should run without errors', () => {
--- src/components/TextField/utils/validators/methods/validateEmailField/validateEmailField.ts
+++ src/components/TextField/utils/validators/methods/validateEmailField/validateEmailField.ts
@@ -2,11 +2,13 @@ import { IError, ISettings } from '@components/TextField/utils/validators';
 
 export const validateEmailField = (
     value: string,
-    settings: ISettings = { template: /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/ }
+    { template }: ISettings = {
+        template: /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/
+    }
 ): IError => {
-    const message: string = value === '' ? 'Пустое поле!' : 'Неверный ввод!';
+    const message: string = value === '' ? 'Пустое поле' : 'Неверный ввод';
 
-    return !value || !settings.template?.test(value)
+    return !value || !template?.test(value)
         ? { isError: true, message }
         : { isError: false, message: '' };
 };
