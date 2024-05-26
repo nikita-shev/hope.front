cedb6ef4f329fb78d43968dfcecef51ab125f054
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-05-26 15:27:39 +0400

    fix: Move stories data to shared area.

--- src/components/cards/PreviewCard/PreviewCard.stories.tsx
+++ src/components/cards/PreviewCard/PreviewCard.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { PreviewCard } from '@components/cards/PreviewCard/PreviewCard.tsx';
+import { product } from '@components/cards/shared/stories/product.ts';
 
 type Story = StoryObj<typeof PreviewCard>;
 
@@ -11,26 +12,7 @@ const meta: Meta<typeof PreviewCard> = {
         layout: 'centered'
     },
     args: {
-        product: {
-            id: '1',
-            name: 'Product Name',
-            description: 'Description',
-            category: '',
-            brand: '',
-            vendorCode: '99 999',
-            price: { current: 200 },
-            isInStock: true,
-            quantity: 10,
-            rating: 5,
-            status: 'sale',
-            colors: [{ name: '', value: '' }],
-            sizes: ['S'],
-            options: null,
-            images: {
-                preview:
-                    'https://i.pinimg.com/originals/c2/15/8d/c2158d1b6ecf3f4493aac61e46b9dd4c.png'
-            }
-        },
+        product,
         style: { minWidth: '270px' }
     }
 };
