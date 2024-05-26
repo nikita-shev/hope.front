e3732ade49d16882982ed66b800ff939c5aee9f3
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-05-26 14:59:18 +0400

    feat[PreviewCard]: Add Price component.

--- src/components/cards/PreviewCard/PreviewCard.tsx
+++ src/components/cards/PreviewCard/PreviewCard.tsx
@@ -2,6 +2,7 @@ import { CSSProperties, ReactElement } from 'react';
 import { IProduct } from '@/types/Product.ts';
 import { Paper } from '@components/Paper';
 import { Button } from '@components/Button';
+import { Price } from '@components/cards/shared/components/Price/Price.tsx';
 import { CartIcon } from '@components/icons';
 import s from '@components/cards/PreviewCard/PreviewCard.module.sass';
 import ls from '@components/cards/shared/styles/cards.module.sass';
@@ -36,13 +37,11 @@ export function PreviewCard({ product, className = '', style }: Props): ReactEle
 
                     <p className={s['card__subtitle']}>Артикул:&nbsp;{product.vendorCode}</p>
 
-                    <div className={`${s['card__price']} ${s.price}`}>
-                        <span className={s['price__text']}>
-                            Цена: <span>₽&nbsp;{product.price.current}.00</span>
-                        </span>
+                    <div className={`${s['card__product-buy']} ${s['product-buy']}`}>
+                        <Price price={product.price} />
 
                         <Button
-                            className={s['price__cart']}
+                            className={s['product-buy__cart']}
                             variant={'contained'}
                             onClick={addProductToCart}>
                             <CartIcon />
