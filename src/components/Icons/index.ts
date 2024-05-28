0c1553f3677c04ec524602f9cb2560944f854101
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-05-28 16:41:15 +0400

    feat: Add LoadingIcon component.

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,4 +1,5 @@
 import { CartIcon } from '@components/icons/components/CartIcon/CartIcon.tsx';
 import { StarIcon } from '@components/icons/components/StarIcon/StarIcon.tsx';
+import { LoadingIcon } from '@components/icons/components/LoadingIcon/LoadingIcon.tsx';
 
-export { CartIcon, StarIcon };
+export { CartIcon, StarIcon, LoadingIcon };
