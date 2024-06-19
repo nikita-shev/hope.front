75abdeffe9deb63b225482c0673f2814a526520a
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-06-19 15:31:13 +0400

    feat: Add Navbar component.

---
+++ src/layout/Navbar/Navbar.tsx
@@ -0,0 +1,34 @@
+import { ReactElement } from 'react';
+import { Button } from '@components/Button';
+import { Search } from '@components/Search';
+import s from '@layout/Navbar/Navbar.module.sass';
+
+export function Navbar(): ReactElement {
+    const menu: { name: string; category: string }[] = [
+        { name: 'Футболки', category: 'Футболки' },
+        { name: 'Шорты', category: 'Шорты' },
+        { name: 'Шапки', category: 'Шапки' }
+    ];
+
+    return (
+        <nav className={s.navbar}>
+            <div className={`${s['navbar__container']} container`}>
+                <Button className={s['navbar__btn']} variant={'text'} onClick={() => {}} />
+
+                <ul className={s['navbar__list']}>
+                    {menu.map((el) => {
+                        return (
+                            <li key={el.name}>
+                                <a className={s['navbar__link']} href="#">
+                                    {el.name}
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+
+                <Search />
+            </div>
+        </nav>
+    );
+}
