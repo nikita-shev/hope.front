061ecb663efcba917bc5a096204c1bba7db276e3
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-07-02 23:16:33 +0400

    chore: Update import/export for Radio component.

--- src/components/Radio/index.ts
+++ src/components/Radio/index.ts
@@ -1,5 +1,2 @@
-import { IRadio } from '@components/Radio/Radio.types.ts';
-import { Radio } from '@components/Radio/Radio.tsx';
-
-export { Radio };
-export type { IRadio };
+export type { IRadio } from '@components/Radio/Radio.types.ts';
+export { Radio } from '@components/Radio/Radio.tsx';
