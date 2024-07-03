6e1c1cdc445bfba80100cd46185859dfb90da36e
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-07-03 23:03:26 +0400

    feat: Add route for Home component.

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '@/App.tsx';
+import { Home } from '@pages/Home';
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <App />
+        element: <App />,
+        children: [
+            {
+                path: '',
+                element: <Home />
+            }
+        ]
     }
 ]);
