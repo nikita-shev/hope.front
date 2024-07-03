9eae94f733ef1d8ece96c9106e856cef141acb6c
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-07-03 23:00:17 +0400

    feat: Add Home component (Homepage).

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { ReactElement } from 'react';
+import { Outlet } from 'react-router-dom';
 import { ModalContext } from '@utils/contexts/ModalContext.ts';
 import { IModalObj, useModal } from '@components/Modal';
 import { Header } from '@layout/Header';
 import { Navbar } from '@layout/Navbar';
-import { ProductsSection } from '@layout/ProductsSection';
 import { Footer } from '@layout/Footer';
-
-import { Preview } from '@layout/Preview';
-import { Subscription } from '@layout/Subscription';
 
 function App(): ReactElement {
     const ModalObj: IModalObj = useModal();
@@ -18,10 +15,7 @@ function App(): ReactElement {
             <Navbar />
 
             <main>
-                <Preview />
-                <ProductsSection title={'Новинки'} status={'new'} />
-                <Subscription />
-                <ProductsSection title={'Распродажа'} status={'sale'} />
+                <Outlet />
             </main>
 
             <Footer />
