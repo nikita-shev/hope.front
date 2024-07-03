8bf2e30aca322d1afc9eee7347ff53c31a437243
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-07-03 22:49:27 +0400

    feat: Add basic router configuration.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router-dom';
 import { store } from '@store/store.ts';
-import App from '@/App.tsx';
+import { router } from '@/router/router.tsx';
 // import '@assets/styles/main.sass';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <RouterProvider router={router} />
         </Provider>
     </React.StrictMode>
 );
