4735701e5a021aa255249c22b1b24f60ee409852
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-07-06 23:03:44 +0400

    feat: Add InStockIcon component.

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -2,3 +2,4 @@ export { CartIcon } from '@components/icons/components/CartIcon/CartIcon.tsx';
 export { StarIcon } from '@components/icons/components/StarIcon/StarIcon.tsx';
 export { LoadingIcon } from '@components/icons/components/LoadingIcon/LoadingIcon.tsx';
 export { NotFoundIcon } from '@components/icons/components/NotFoundIcon/NotFoundIcon.tsx';
+export { InStockIcon } from '@components/icons/components/InStockIcon/InStockIcon.tsx';
