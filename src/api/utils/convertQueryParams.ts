573e79d1ca3dc20f398570c35bce497ef2f28449
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-07-18 20:57:05 +0400

    feat[api]: Add string support to convertQueryParams() method.

--- src/api/utils/convertQueryParams.ts
+++ src/api/utils/convertQueryParams.ts
@@ -1,4 +1,7 @@
 import { IQuery } from '@/api';
 
-export const convertQueryParams = (query: IQuery): string =>
-    new URLSearchParams(Object.entries(query)).toString();
+export const convertQueryParams = (query: string | IQuery): string => {
+    return typeof query === 'string'
+        ? query
+        : new URLSearchParams(Object.entries(query)).toString();
+};
