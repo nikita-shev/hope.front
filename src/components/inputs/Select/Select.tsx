81d42a68f6db387e61ae106947ec5739530c74dc
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-08-01 18:53:14 +0400

    feat[Select]: Add value passing to parent component.

--- src/components/inputs/Select/Select.tsx
+++ src/components/inputs/Select/Select.tsx
@@ -1,26 +1,42 @@
-import { CSSProperties, ReactElement, useState } from 'react';
-import { IOption } from '@components/inputs/Select/Select.types.ts';
+import { CSSProperties, ReactElement, useEffect, useState } from 'react';
+import { IOption, IReturnValue } from '@components/inputs/Select/Select.types.ts';
 import { useListVisibility } from '@components/inputs/Select/hooks/useListVisibility.ts';
 import { Button } from '@components/inputs/Button';
 import s from '@components/inputs/Select/Select.module.sass';
 
 interface Props {
     options: IOption[];
+    selected?: number; // rename
     children?: ReactElement;
     className?: string;
     style?: CSSProperties;
+    onChange: (value: IReturnValue) => void;
 }
 
-export function Select({ options, children, className = '', style }: Props): ReactElement {
+export function Select({
+    options,
+    selected,
+    children,
+    className = '',
+    style,
+    onChange
+}: Props): ReactElement {
     const [selectedOption, setSelectedOption] = useState<IOption>(options[0]);
     const [isShowList, setIsShowList] = useState<boolean>(false);
     const [headerRef, listBtnRef] = useListVisibility(isShowList, () => setIsShowList(!isShowList));
+
+    useEffect(() => {
+        const option = options.find((el) => el.order === selected);
+        option && setSelectedOption(option);
+    }, [selected]);
 
     const toggleDisplay = (): void => setIsShowList(!isShowList);
 
     const selectOption = (option: IOption): void => {
         setSelectedOption(option);
         toggleDisplay();
+
+        onChange({ value: option.order ?? 0 });
     };
 
     const list: ReactElement[] = options
