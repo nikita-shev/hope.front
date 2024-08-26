e65a5dbbe6b1854d9306f99126646a903ee93804
Nikita Shevchenko <nikita.shevchenko.web@gmail.com>
2024-08-26 21:44:35 +0400

    feat: Add new TextField component.

--- src/components/inputs/TextField/TextField.tsx
+++ src/components/inputs/TextField/TextField.tsx
@@ -1,64 +1,51 @@
 import { ChangeEvent, ReactElement } from 'react';
-import { Control, HTMLElements, ITextField } from '@components/inputs/TextField/TextField.types.ts';
-import { Input } from '@components/inputs/TextField/components/Input.tsx';
-import { Textarea } from '@components/inputs/TextField/components/Textarea.tsx';
+import { ITextField } from '@components/inputs/TextField/TextField.types.ts';
 import { validateField } from '@components/inputs/TextField/utils/validateField/validateField.ts';
 import { formatPhoneNumber } from '@components/inputs/TextField/utils/formatPhoneNumber/formatPhoneNumber.ts';
 import s from '@components/inputs/TextField/TextField.module.sass';
 
-interface IControls {
-    [key: string]: Control;
-}
-
 interface Props extends ITextField {
+    onChange: (value: string, id?: string, isValid?: boolean) => void;
     isSubmit?: boolean;
-    onChange: (id: string, value: string, isValid: boolean) => void;
 }
 
 export function TextField({
-    id,
-    variant,
-    value,
-    validation,
     type,
     name,
-    className,
+    value,
+    id,
+    validation,
+    className = '',
     style,
     placeholder,
     isSubmit,
-    onChange,
-    ...props
+    onChange
 }: Props): ReactElement {
     const { isError, message } = validateField(validation, value);
-    const isShowError: boolean = Boolean(isSubmit) && isError;
+    const isShowError: boolean = Boolean(validation) && Boolean(isSubmit) && isError;
+    const combinedClassNames: string = `${className} ${s.input}`.trim();
 
-    const controls: IControls = {
-        standard: Input,
-        multiline: Textarea
-    };
-    const Control: Control = controls[variant];
-
-    const changeValue = (e: ChangeEvent<HTMLElements>): void => {
+    const changeValue = (e: ChangeEvent<HTMLInputElement>): void => {
         const currentValue: string = e.currentTarget.value;
         const newValue: string = type === 'tel' ? formatPhoneNumber(currentValue) : currentValue;
         const { isError } = validateField(validation, newValue);
 
-        onChange(id, newValue, !isError);
+        onChange(newValue, id, !isError);
     };
 
-    const onFocusHandler = (e: ChangeEvent<HTMLElements>): void => changeValue(e);
+    const onFocusHandler = (e: ChangeEvent<HTMLInputElement>): void => changeValue(e);
 
-    const onBlurHandler = (e: ChangeEvent<HTMLElements>): void => {
+    const onBlurHandler = (e: ChangeEvent<HTMLInputElement>): void => {
         const currentValue: string = e.currentTarget.value;
         const newValue: string = type === 'tel' && currentValue.length < 5 ? '' : currentValue;
 
-        onChange(id, newValue, !isError);
+        onChange(newValue, id, !isError);
     };
 
     return (
         <div className={s['text-field']} style={style?.wrap}>
-            <Control
-                className={className}
+            <input
+                className={combinedClassNames}
                 style={style?.input}
                 type={type}
                 name={name}
@@ -67,7 +54,6 @@ export function TextField({
                 onChange={changeValue}
                 onFocus={onFocusHandler}
                 onBlur={onBlurHandler}
-                {...props}
             />
 
             {isShowError && (
